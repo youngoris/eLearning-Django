d457806e1bdd5744c369b752e3f0bb526a921e04
Andre Ho <andre.ho@gmx.com>
2024-03-05 01:24:44 +0800

    really tired

--- apps/main/views.py
+++ apps/main/views.py
@@ -5,24 +5,31 @@ from apps.accounts.models import CustomUser
 from django.db.models import Q
 
 def home(request):
-    courses = Course.objects.all()[:5]  # 举例，获取最新的5门课程
+
+    courses = Course.objects.all()[:5]  # 假设你只想获取最新的5门课程
+    print(courses)  # 打印查询到的课程列表
     context = {'courses': courses}
-
-
-    if request.user.is_authenticated:
-        if hasattr(request.user, 'teacher_profile'):
-            # 为教师用户准备的上下文
-            teacher_courses = request.user.authored_courses.all()
-            context.update({'teacher_courses': teacher_courses})
-        elif hasattr(request.user, 'student_profile'):
-            # 为学生用户准备的上下文
-            student_profile = request.user.student_profile
-            student_user = student_profile.user  # 获取 CustomUser 实例
-            student_enrollments = Enrollment.objects.filter(student=student_user)
-            context.update({'student_enrollments': student_enrollments})
-
-
     return render(request, 'main/home.html', context)
+
+
+    # courses = Course.objects.all()[:5]  # 举例，获取最新的5门课程
+    # context = {'courses': courses}
+
+
+    # if request.user.is_authenticated:
+    #     if hasattr(request.user, 'teacher_profile'):
+    #         # 为教师用户准备的上下文
+    #         teacher_courses = request.user.authored_courses.all()
+    #         context.update({'teacher_courses': teacher_courses})
+    #     elif hasattr(request.user, 'student_profile'):
+    #         # 为学生用户准备的上下文
+    #         student_profile = request.user.student_profile
+    #         student_user = student_profile.user  # 获取 CustomUser 实例
+    #         student_enrollments = Enrollment.objects.filter(student=student_user)
+    #         context.update({'student_enrollments': student_enrollments})
+
+
+    # return render(request, 'main/home.html', context)
 
 def search_results(request):
     query = request.GET.get('query', '')
